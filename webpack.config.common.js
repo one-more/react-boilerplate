e39a08243153dcb82604d1a2db94c5391bdf9427
one-more <greyjoi@yandex.ru>
2019-08-30 15:05:39 +0300

    webpack tilda alias

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -4,7 +4,10 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry: './src/index.tsx',
     resolve: {
-        extensions: ['.ts', '.tsx', '.js']
+        extensions: ['.ts', '.tsx', '.js'],
+        alias: {
+            '~': path.resolve(__dirname, 'src/'),
+        }
     },
     output: {
         path: path.join(__dirname, '/dist'),
